59c56c8d102d05366c19e25931b512a7f7ae6292
graell <gresyaleman12@gmail.com>
2022-11-09 20:20:27 +0700

    chapter 3 done

--- app.py
+++ app.py
@@ -14,4 +14,8 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    #return jsonify("TODO")
+    #return "Hello, World!"
+
+    response = { "data" : "Hello, World!" } 
+    return jsonify(response)
